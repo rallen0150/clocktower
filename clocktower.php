436d2ecd47c12758c4175e422c2f744bcc8cff3a
rallen0150 <rallen0150@gmail.com>
2018-11-01 18:48:59 -0400

    fixed bugs and changed to 24 hr format

--- clocktower.php
+++ clocktower.php
@@ -2,42 +2,72 @@
 // function to get the count of bell chimes so I don't repeat this code
 function checkHour ($startHour, $endHour) {
   $count = 0;
-  for ($startHour = intval($startHour); $startHour <= 13; $startHour++) {
-    if ($startHour == 13) {
-      $startHour = 1;
+  for ($startHour = intval($startHour); $startHour <= 24; $startHour++) {
+    if ($startHour == 24) {
+      $startHour = 0;
     }
-
-    $count+=$startHour;
+    // If it's midnight (or 12 am), then the clock hour should be 12 and not 0
+    if ($startHour == 0) {
+      $time = 12;
+      $count+=$time;
+    }
+    // If it's past noon and before midnight, the chimes should be equivalent to 12 hour format
+    if ($startHour > 12) {
+      $time = $startHour-12;
+      $count+=$time;
+    } else {
+      $count+=$startHour;
+    }
     if ($startHour == $endHour) {
       break;
     }
+
   }
   return $count;
+
 }
+
+// echo checkHour(22, 20);
+// echo intval(date('H', strtotime('24:00')));
 
 function countBells($startTime, $endTime) {
   /*
+    Get the hour and minute of each time for comparison
+  */
+  $startHour = date('H', strtotime($startTime));
+  $startMin = date('i', strtotime($startTime));
+  $endHour = date('H', strtotime($endTime));
+  $endMin = date('i', strtotime($endTime));
+  /*
     If the times are the same, then it acts as 24 hours has passed, so each chime is for the hour the hour-hand is on
-    The math goes ((1+2+3+4+5+6+7+8+9+10+11+12)*2) because it's adding each hour pass has the number of chimes equivalent to what hour the hour hand is on
+    The math goes ((1+2+3+4+5+6+7+8+9+10+11+12)*2) + hour it's on, because it's adding each hour pass has the number of chimes equivalent to what hour the hour hand is on
   */
   if ($startTime === $endTime) {
-    $count = 156;
+    $time = $startHour;
+    // If it's past noon and before midnight, the chimes should be equivalent to 12 hour format
+    if ($time > 12) {
+      $time-=12;
+    }
+    $count = 156+$time;
     return $count."\n";
   }
-  /*
-    Get the hour and minute of each time for comparison
-    Each hour is not on a 24 hour format, it's on a 12 hour format
-  */
-  $startHour = date('g', strtotime($startTime));
-  $startMin = date('i', strtotime($startTime));
-  $endHour = date('g', strtotime($endTime));
-  $endMin = date('i', strtotime($endTime));
 
   // If the startMin is greater than the endMin, run the checkHour function then erase the start hour if it didn't start on the top of the hour (which should always be the case)
   if ($startMin > $endMin) {
     $count = checkHour($startHour, $endHour);
     if ($startMin > '00') {
-      $count -= $startHour;
+      // If it's midnight (or 12 am), then the clock hour should be 12 and not 0
+      if ($startHour == 0) {
+        $time = 12;
+        $count -= $time;
+      }
+      // If it's past noon and before midnight, the chimes should be equivalent to 12 hour format
+      if ($startHour > 12) {
+        $time = $startHour-12;
+        $count -= $time;
+      } else {
+        $count -= $startHour;
+      }
     }
   }
   /*
@@ -46,7 +76,15 @@ function countBells($startTime, $endTime) {
   else {
     $count = checkHour($startHour, $endHour);
     if ($startMin > '00') {
-      $count -= $startHour;
+      if ($startHour == 0) {
+        $time = 12;
+        $count -= $time;
+      }
+      if ($startHour > 12) {
+        $time = $startHour-12;
+        $count -= $time;
+      }
+
     }
   }
   return $count."\n";
@@ -56,6 +94,8 @@ echo countBells('2:00', '3:00'); // output 5
 echo countBells('14:00', '15:00'); // output 5
 echo countBells('14:23', '15:42'); // output 3
 echo countBells('23:00', '1:00'); // output 24
-// echo countBells('14:52', '16:42'); // output 7
-// echo countBells('22:00', '22:00'); // output 156
+// echo countBells('23:00', '21:00'); // output 146
+// echo countBells('14:32', '16:42'); // output 7
+// // echo countBells('11:52', '16:42'); // output 22
+// // echo countBells('22:00', '22:00'); // output 166 (156+10)
 ?>
